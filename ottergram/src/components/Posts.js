dbadaa8519337a3f1a5c8536089ea11571fe87d2
Mike Benfield <Benfield.Michael@mayo.edu>
2022-11-07 12:46:18 -0600

    Added user events

--- ottergram/src/components/Posts.js
+++ ottergram/src/components/Posts.js
@@ -1,9 +1,12 @@
 // import Barry from '../otters/otter1.jpg'
 
 function Post({ image, name}) {
+    const handleClick = (otterName) => {
+        return console.log('You Clicked ' + otterName)
+    }
     return (
         <li className='post-component'>
-            <button>
+            <button onClick={() => handleClick(name)}>
                 <img src={image} alt={name}/>
                 <p className='post-name'>{name}</p>
             </button>
